27b0e177f87cd42e87e23554508bc5716dfda54c
u2021 <jdgatari@upandgo.fr>
2021-08-02 17:40:00 +0200

    feat: add dataBase queries and some global constants

---
+++ config/constants.js
@@ -0,0 +1,4 @@
+const NAMES_LIMIT = 300;
+const GENDER_API_KEY =  "P37ErlesXUn2Q8oSxmwKlxxZhAkUZQSWkFwr";
+
+module.exports ={NAMES_LIMIT,GENDER_API_KEY}
--- database/db.js
+++ database/db.js
@@ -1,15 +1,46 @@
 const mongoose = require("mongoose");
+const constants = require("../config/constants");
+const fs = require("fs");
+const readline = require("readline");
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(`MongoDB connected:${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected:${conn.connection.host}`);
     } catch (error) {
-      console.error(`Connection failed: ${error}`);
-      process.exit(1);
+        console.error(`Connection failed: ${error}`);
+        process.exit(1);
     }
-  };
-  module.exports = { connectDB};
+};
+
+
+const createData = (filePath) => {
+    processLineByLine(filePath);
+};
+
+async function processLineByLine(filePath) {
+    const fileStream = fs.createReadStream(filePath);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+    });
+    var namesCount = 0;
+    for await (const line of rl) {
+        const firstName = line.split(" ")[0];
+        namesCount += 1;
+        if (namesCount <= constants.NAMES_LIMIT) {
+            saveName(firstName);
+        }
+    }
+}
+
+const saveName = (firstName) => {
+    new FirstName({ firstName: firstName }).save((err, data) => {
+        if (err) throw err;
+    });
+};
+
+module.exports = { connectDB , createData };
