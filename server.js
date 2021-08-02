0d9a4b391b8e56fde859dd86feab96675f88dcf1
u2021 <jdgatari@upandgo.fr>
2021-08-02 18:11:19 +0200

    manage user form actions

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", async function (req, res) {
       res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
         randomFirstName: randomFirstName,
         score: constants.INITIAL_POINTS,
-        hasStarted: hasStarted,
+   
       });
     });
   });
@@ -31,3 +31,28 @@ app.listen(
     console.log(`SERVER running in ${process.env.NODE_ENV}
       mode on port  ${PORT}`)
   );
+
+  app.post("/", async (req, res) => {
+    var score = parseInt(req.body.score);
+    let fName = req.body.firstname;
+  
+    genderApiClient.getByFirstName(fName, async function (response) {
+      if (response.gender === req.body.gender) {
+        score += 1;
+      } else if (req.body.gender === "mixed"){
+          score = constants.MIXED_VALUE > response.accuracy ? score + 1 : score - 1; 
+      }else{
+        score -= 1;
+        console.log("score", score);
+      }
+  
+      await db.getRandomFirstName(FirstName).then((randomFirstName) => {
+        res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
+          randomFirstName: randomFirstName,
+          score: score,
+       
+  
+        });
+      });
+    });
+  });
