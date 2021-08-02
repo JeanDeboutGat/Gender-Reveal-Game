c54c5855a71014104dececd724d3b082c4451534
u2021 <jdgatari@upandgo.fr>
2021-08-02 18:54:41 +0200

    feat: treat restart game option

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const path = require("path");
 const constants = require("./config/constants");
 const GenderApi = require("gender-api.com-client");
 
-
 const gameHasStarted = false;
-var genderApiClient = new GenderApi.Client(
-  constants.GENDER_API_KEY);
-  
 
 dotenv.config({ path: "./config/config.env" });
 db.connectDB();
@@ -23,42 +19,57 @@ app.set("view engine", "ejs");
 
 app.get("/", async function (req, res) {
     await db.getRandomFirstName(FirstName).then((randomFirstName) => {
-      res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
-        randomFirstName: randomFirstName,
-        score: constants.INITIAL_POINTS,
-        gameHasStarted: gameHasStarted
-      });
+        res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
+            randomFirstName: randomFirstName,
+            score: constants.INITIAL_POINTS,
+            gameHasStarted: gameHasStarted
+        });
     });
-  });
+});
 
+
+var genderApiClient = new GenderApi.Client(
+    constants.GENDER_API_KEY);
+
+app.post("/", async (req, res) => {
+    var score = parseInt(req.body.score);
+    let fName = req.body.firstname;
+
+    genderApiClient.getByFirstName(fName, async function (response) {
+        if (response.gender === req.body.gender) {
+            score += 1;
+        } else if (req.body.gender === "mixed") {
+            score = constants.MIXED_VALUE > response.accuracy ? score + 1 : score - 1;
+        } else {
+            score -= 1;
+            console.log("score", score);
+        }
+
+        await db.getRandomFirstName(FirstName).then((randomFirstName) => {
+            res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
+                randomFirstName: randomFirstName,
+                score: score,
+                gameHasStarted: gameHasStarted,
+
+            });
+        });
+    });
+});
+
+
+app.get("/restart", async function (req, res) {
+    await db.getRandomFirstName(FirstName).then((randomFirstName) => {
+        res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
+            randomFirstName: randomFirstName,
+            score: constants.INITIAL_POINTS,
+            gameHasStarted: gameHasStarted
+
+        });
+    });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(
     PORT,
     console.log(`SERVER running in ${process.env.NODE_ENV}
       mode on port  ${PORT}`)
-  );
-
-  app.post("/", async (req, res) => {
-    var score = parseInt(req.body.score);
-    let fName = req.body.firstname;
-  
-    genderApiClient.getByFirstName(fName, async function (response) {
-      if (response.gender === req.body.gender) {
-        score += 1;
-      } else if (req.body.gender === "mixed"){
-          score = constants.MIXED_VALUE > response.accuracy ? score + 1 : score - 1; 
-      }else{
-        score -= 1;
-        console.log("score", score);
-      }
-  
-      await db.getRandomFirstName(FirstName).then((randomFirstName) => {
-        res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
-          randomFirstName: randomFirstName,
-          score: score,
-          gameHasStarted: gameHasStarted,
-  
-        });
-      });
-    });
-  });
+);
