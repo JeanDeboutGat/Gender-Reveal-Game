b60a1aac1f2d828ba6dfe5e0d473fc9e72b87e6f
u2021 <jdgatari@upandgo.fr>
2021-08-02 18:09:17 +0200

    feat: retrieve data randomly from database

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./database/db");
 const FirstName = require("./models/name");
+const path = require("path");
+
 
 
 dotenv.config({ path: "./config/config.env" });
 db.connectDB();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("view engine", "ejs");
+
+app.get("/", async function (req, res) {
+    await db.getRandomFirstName(FirstName).then((randomFirstName) => {
+      res.render(`${path.join(__dirname, "views", "index.ejs")}`, {
+        randomFirstName: randomFirstName,
+        score: constants.INITIAL_POINTS,
+        hasStarted: hasStarted,
+      });
+    });
+  });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(
     PORT,
     console.log(`SERVER running in ${process.env.NODE_ENV}
