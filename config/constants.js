76834a4435c9061c3028a32a500606bb767c65b5
u2021 <jdgatari@upandgo.fr>
2021-08-02 18:06:49 +0200

    feat: add global constants

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,6 @@
 const NAMES_LIMIT = 300;
-const GENDER_API_KEY =  "P37ErlesXUn2Q8oSxmwKlxxZhAkUZQSWkFwr";
+const INITIAL_POINTS = 10;
+const GENDER_API_KEY =   "P37ErlesXUn2Q8oSxmwKlxxZhAkUZQSWkFwr";
+const MIXED_VALUE = 60;
 
-module.exports ={NAMES_LIMIT,GENDER_API_KEY}
+module.exports ={NAMES_LIMIT, INITIAL_POINTS, GENDER_API_KEY, MIXED_VALUE}
